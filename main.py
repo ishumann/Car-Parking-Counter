3e6a7531177592adc0b58fc743e8abf9d5776e73
ishumann <mann.agrawal17@gmail.com>
2024-12-31 21:19:37 +0530

    Refactor main pipeline: replace data validation stage with PosFinder stage and update logging

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from carParkingCounter.pipeline.data_ingestion_stage import (
     DataIngestionTrainingPipeline,
 )
 from carParkingCounter.pipeline.data_validation_stage import (
-    DataValidationTrainingPipeline,
+    DataValidationTrainingPipeline, 
 )
-# from carParkingCounter.pipeline.stage_03_data_transformation import (
-#     DataTransformationTrainingPipeline,
-# )
+from carParkingCounter.pipeline.posfinder import (
+    PosFinderPipeline
+)
 # from carParkingCounter.pipeline.stage_04_model_pipeline import ModelTrainingPipeline
 # from carParkingCounter.pipeline.stage_05_model_evaluation_pipeline import (
 #     ModelEvaluationPipeline,
@@ -26,12 +26,25 @@ from carParkingCounter import logger
 #     raise e
 
 
-STAGE_NAME = "Date Validation"
+# STAGE_NAME = "Date Validation"
+
+# try:
+#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<<")
+#     data_validation = DataValidationTrainingPipeline()
+#     data_validation.main()
+#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+
+STAGE_NAME = "PosFinder"
 
 try:
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<<")
-    data_validation = DataValidationTrainingPipeline()
-    data_validation.main()
+    posfinder = PosFinderPipeline()
+    posfinder.main()
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
