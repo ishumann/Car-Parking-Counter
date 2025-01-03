fa3f6f2a63a4cbe8a13e1db466c09e6d76886a32
ishumann <mann.agrawal17@gmail.com>
2025-01-03 14:58:29 +0530

    Update ParkingCounter functionality and configuration: modify video output path in config.yaml, implement video processing in ParkingCounter class, and streamline pipeline to utilize new counting method.

--- src/CarParkingCounter/components/parking_counter.py
+++ src/CarParkingCounter/components/parking_counter.py
@@ -5,7 +5,7 @@ from carParkingCounter.utils.common import load_pickle
 from carParkingCounter.entity import ParkingCounterConfig
 from pathlib import Path
 from box import ConfigBox
-
+import numpy as np
 # box_width = 108
 # box_height = 47
 
@@ -20,8 +20,6 @@ class ParkingCounter:
 
         # with open('carparkpos', 'rb') as f:
         #     posList = pickle.load(f)
-
-        
 
     def checkparkingspace(self, preprocessed_frame, frame):
         counter=0
@@ -61,3 +59,61 @@ class ParkingCounter:
                     thickness=1,
                     lineType=cv2.LINE_AA,
                 )
+
+    def save_video_out(self, cap):
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        fourcc = cv2.VideoWriter_fourcc(*"hvc1")  # Codec for .mp4 files
+        out = cv2.VideoWriter(Path(self.config.video_write_dir), fourcc, fps, (frame_width, frame_height))
+        return out
+
+    def countemptyspace(self):
+        cap = cv2.VideoCapture(Path(self.config.video_dir))
+        out = self.save_video_out(cap)
+        while True:
+            # For Video loop
+            # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            #     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+            ret, frame = cap.read()
+
+            if ret:
+                # Convert our frame to gray scale
+                gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                # Add Gaussian Blur
+                blur = cv2.GaussianBlur(gray_scale, (3, 3), 1)
+
+                # Applying threshold on each of the frame of the video
+
+                frame_threshold = cv2.adaptiveThreshold(
+                    blur,
+                    255,
+                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                    cv2.THRESH_BINARY_INV,
+                    25,
+                    16,
+                )
+
+                # To remove the noise dots ws will apply median blur
+
+                median_blur = cv2.medianBlur(frame_threshold, 5)
+
+                kernel = np.ones((3, 3), np.uint8)
+                frame_dilate = cv2.dilate(median_blur, kernel, iterations=1)
+
+                self.checkparkingspace(frame_dilate, frame)
+
+                cv2.imshow("video", frame)
+                # Write the processed frame to the output video
+                processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                out.write(cv2.cvtColor(processed_frame, cv2.COLOR_GRAY2BGR))
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            else:
+                break
+
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
--- src/CarParkingCounter/pipeline/parking_counter.py
+++ src/CarParkingCounter/pipeline/parking_counter.py
@@ -15,41 +15,5 @@ class ParkingCounterPipeline:
         config = ConfigurationManager()
         parking_counter_config = config.get_parking_counter_config()
         parking_counter = ParkingCounter(config=parking_counter_config)
-
-        cap = cv2.VideoCapture(Path(parking_counter_config.video_dir))
-        while True:
-            #For Video loop
-            # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            #     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-            ret, frame = cap.read()
-
-            if ret:
-                # Convert our frame to gray scale
-                gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-                # Add Gaussian Blur
-                blur = cv2.GaussianBlur(gray_scale, (3, 3), 1)
-
-                # Applying threshold on each of the frame of the video
-
-                frame_threshold = cv2.adaptiveThreshold(
-                    blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16
-                )
-
-                # To remove the noise dots ws will apply median blur
-
-                median_blur = cv2.medianBlur(frame_threshold, 5)
-
-                kernel = np.ones((3, 3), np.uint8)
-                frame_dilate = cv2.dilate(median_blur, kernel, iterations=1)
-
-                parking_counter.checkparkingspace(frame_dilate, frame)
-
-                cv2.imshow("video", frame)
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
-            else:
-                break
-
-        cap.release()
+        parking_counter.countemptyspace()
+        
